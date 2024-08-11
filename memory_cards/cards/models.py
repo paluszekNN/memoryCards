384769406f89b1c3b2e06990d40646eeff1dc33d
paluszekNN <adampaluch95@gmail.com>
2024-08-11 17:04:38 +0200

    add CardLog and Deck

--- memory_cards/cards/models.py
+++ memory_cards/cards/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
 
+class Deck(models.Model):
+    name = models.CharField(max_length=1000)
+
+
 class Card(models.Model):
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
     question_text = models.CharField(max_length=1000)
     answer_text = models.CharField(max_length=1000)
     association_text = models.CharField(max_length=1000)
     last_remembered = models.DateTimeField("last remembered", default=None, null=True)
     experience = models.FloatField(default=0)
+
+
+class CardLog(models.Model):
+    card = models.ForeignKey(Card, on_delete=models.CASCADE)
+    association_text = models.CharField(max_length=1000)
+    time_diff_min = models.FloatField()
+    is_good = models.BooleanField()
