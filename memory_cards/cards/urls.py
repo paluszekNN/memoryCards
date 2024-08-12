3ed34736620f7f499661914bc9e344a0e991f72c
paluszekNN <adampaluch95@gmail.com>
2024-08-12 21:04:44 +0200

    add cards and decks views

--- memory_cards/cards/urls.py
+++ memory_cards/cards/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('add_deck', views.add_deck, name='add_deck'),
     path('add_deck_form', views.add_deck_form, name='add_deck_form'),
+    path('', views.IndexView.as_view(), name='decks'),
+    path('cards', views.CardsView.as_view(), name='cards'),
 ]
--- memory_cards/cards/views.py
+++ memory_cards/cards/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Deck, Card, CardLog
+from django.shortcuts import redirect
+from django.views import generic
+from django.db.models import Q
 
 
 def add_deck(request):
@@ -20,3 +23,22 @@ def add_deck_form(request):
     deck = Deck(**kwargs)
     deck.save()
     return HttpResponse(template.render({}, request))
+
+
+class CardsView(generic.ListView):
+    template_name = 'cards/cards.html'
+    context_object_name = 'deck'
+
+    def get_queryset(self):
+        deck = Deck.objects.filter(Q(id__icontains=self.request.GET.get('q')))[0]
+        return Card.objects.filter(Q(deck=deck))
+
+
+class IndexView(generic.ListView):
+    template_name = 'cards/decks.html'
+    context_object_name = 'decks'
+
+    def get_queryset(self):
+        order_by = 'name'
+        # return Deck.objects.order_by(order_by), UserDeck.objects.filter(Q(user=self.request.user)).order_by(order_by)
+        return Deck.objects.order_by(order_by)
