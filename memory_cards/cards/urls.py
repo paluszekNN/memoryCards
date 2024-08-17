37f7fd692ed6d17c84679928b99ad986ea73bb7a
paluszekNN <adampaluch95@gmail.com>
2024-08-17 16:56:00 +0200

    add form to add cards

--- memory_cards/cards/urls.py
+++ memory_cards/cards/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('add_deck_form', views.add_deck_form, name='add_deck_form'),
+    path('add_card_form', views.add_card_form, name='add_card_form'),
     path('', views.IndexView.as_view(), name='decks'),
     path('cards', views.CardsView.as_view(), name='cards'),
     path('delete_deck/<str:deck_name>/', views.deck_delete, name='deck_delete'),
--- memory_cards/cards/views.py
+++ memory_cards/cards/views.py
@@ -19,13 +19,35 @@ def add_deck_form(request):
     return redirect('decks')
 
 
+def add_card_form(request):
+    template = loader.get_template('cards/cards.html')
+    deck_id = request.POST["deck_id"]
+    question_text = request.POST["question_text"]
+    answer_text = request.POST["answer_text"]
+    association_text = request.POST["association_text"]
+    kwargs = {
+        'deck_id': deck_id,
+        'question_text': question_text,
+        'answer_text': answer_text,
+        'association_text': association_text
+              }
+    card = Card(**kwargs)
+    card.save()
+    return redirect(reverse('cards') + '?q=' + deck_id)
+
+
 class CardsView(generic.ListView):
     template_name = 'cards/cards.html'
-    context_object_name = 'deck'
+    context_object_name = 'cards'
 
     def get_queryset(self):
         deck = Deck.objects.filter(Q(id__icontains=self.request.GET.get('q')))[0]
         return Card.objects.filter(Q(deck=deck))
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['deck_id'] = self.request.GET.get('q')
+        return context
 
 
 class IndexView(generic.ListView):
