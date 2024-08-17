f0a3acedb1e0da1a9c0d942b6632d3c341f44172
paluszekNN <adampaluch95@gmail.com>
2024-08-17 12:24:56 +0200

    move add deck to decks site

--- memory_cards/cards/urls.py
+++ memory_cards/cards/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add_deck', views.add_deck, name='add_deck'),
     path('add_deck_form', views.add_deck_form, name='add_deck_form'),
     path('', views.IndexView.as_view(), name='decks'),
     path('cards', views.CardsView.as_view(), name='cards'),
--- memory_cards/cards/views.py
+++ memory_cards/cards/views.py
@@ -6,23 +6,12 @@ from django.views import generic
 from django.db.models import Q
 
 
-def add_deck(request):
-    # if not request.user.is_superuser:
-    #     return HttpResponseForbidden('Nope!')
-    template = loader.get_template('cards/add_deck.html')
-    # print(request.user.last_login)
-    return HttpResponse(template.render({}, request))
-
-
 def add_deck_form(request):
-    template = loader.get_template('cards/add_deck.html')
     name = request.POST["name"]
-    # print(request.FILES)
-
     kwargs = {'name': name}
     deck = Deck(**kwargs)
     deck.save()
-    return HttpResponse(template.render({}, request))
+    return redirect('decks')
 
 
 class CardsView(generic.ListView):
